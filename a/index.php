c5a33eef9dbbfebba13333bd89a9064c1b957cea
wungsai <wslvc8492@gmail.com>
2021-10-20 10:31:25 +0700

    fix error

--- a/index.php
+++ a/index.php
@@ -1,6 +1,6 @@
 <?php echo "hellow wampire ";
 //open windows text file by directory path
-$txt = fopen("C:\\xampp\\htdocs\\a\\b\\a.txt", "r");
+$txt = fopen("C:\\xampp\\htdocs\\php\\a\\b\\a.txt", "r");
 $s = fgets($txt);
 echo "</br>";
 echo $s;
@@ -12,9 +12,9 @@ echo date('YmdHis').substr(round(microtime(true)*100),-2);
 echo "</br>";
 
 //show windows directory path
-$dir = "/a/b/";
+$dir = "/php/a/b/";
 $a = dir(getcwd());
-$d = "{$a->path}{$dir}";
+
 echo "</br>";
 $dirPath = $a->path;
 $checkFile = scandir($dirPath);
@@ -28,7 +28,7 @@ echo $newDir;
 echo "</br>";
 echo "replace: {$i}";
 echo "</br>";
-echo $d;
+
 echo "</br>";
 $use = "{$_SERVER['DOCUMENT_ROOT']}{$dir}";
 $test = scandir($use);
