695648ea37604f821625fbd7c1d5ecdbf0df3e66
wungsai <wslvc8492@gmail.com>
2021-10-20 12:04:31 +0700

    update creating setName and collecting files name to set

--- a/index.php
+++ a/index.php
@@ -12,7 +12,7 @@ echo date('YmdHis').substr(round(microtime(true)*100),-2);
 echo "</br>";
 
 //show windows directory path
-$dir = "/php/a/b/";
+$dir = "/php/a/b/sticker/";
 $a = dir(getcwd());
 
 echo "</br>";
@@ -30,8 +30,43 @@ echo "replace: {$i}";
 echo "</br>";
 
 echo "</br>";
+//list all files names here
 $use = "{$_SERVER['DOCUMENT_ROOT']}{$dir}";
 $test = scandir($use);
 print_r($test);
+$stickerSet= array();
 echo "</br>";
+//collect set Name
+$setName = array();
+foreach($test as $t){
+if(!is_numeric(strpos($t,"."))){
+    echo "</br> {$t}";
+    $setName[] = $t;
+}
+
+}
+echo "</br>";
+print_r($setName);
+echo "</br>";
+//create set Name for files group
+foreach ($setName as $set) {
+    $nameSet = array();
+    $names = scandir("{$_SERVER['DOCUMENT_ROOT']}/php/a/b/sticker/{$set}");
+    foreach ($names as $name) {
+       if(is_numeric(strpos($name,".svg"))) {
+           echo $name;
+           $nameSet[] = $name;
+        echo "</br>";}
+       
+    }
+
+    //collected files name in Set Name
+    $stickerSet[] = [$set => $nameSet];
+}
+echo "</br></br></br></br></br></br></br></br>Use</br></br></br></br></br>";
+var_dump($stickerSet);
+
+$j=json_encode($stickerSet);
+echo "</br>";
+var_dump($j);
 ?>
