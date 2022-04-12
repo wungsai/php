3a50ae10ad19fb14e04e9af445719b84f3513a0a
wungsai <wslvc8492@gmail.com>
2022-04-12 10:15:06 +0700

    update class path

--- class/classes/json.php
+++ class/classes/json.php
@@ -1,16 +1,24 @@
-<?php 
-include_once("test.php");
-class UseJson {
-     var $data;
+<?php
+
+foreach (glob("classes/*.php") as $file) {
+    echo $file;
+    echo "<br>";
+    include_once($file);
+}
+
+class UseJson
+{
+    var $data;
     //  var $json = json_decode($data);
-function showName($data) {
-     $json = json_decode($data);
-    echo $json->name;
-}
+    function showName($data)
+    {
+        $json = json_decode($data);
+        echo $json->name;
+    }
 
-function showTest($data) {
-     $test = new Test();
-    $test->showTest($data);
-}
-
+    function showTest($data)
+    {
+        $test = new Test();
+        $test->showTest($data);
+    }
 }
